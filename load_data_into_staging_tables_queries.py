a5fe4ef261361dc78cda750aab80d4c97f0ae5e3
Joseph Corrado <josephcorrado7@gmail.com>
2021-01-18 15:20:55 -0500

    Initial commit for module that defines the SQL queries to load the data fro the S3 buckets into staging tables.

---
+++ load_data_into_staging_tables_queries.py
@@ -0,0 +1,33 @@
+"""This module defines the queries to copy the data from the S3 buckets into the
+staging tables
+"""
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+table_names_config = configparser.ConfigParser()
+table_names_config.read("table_names.cfg")
+
+staging_events_copy = ("""
+COPY {} 
+FROM '{}'
+CREDENTIALS 'aws_iam_role={}'
+REGION 'US-WEST-2';
+""").format(table_names_config["STAGING"]["events"],
+            config["S3"]["LOG_DATA"],
+            config["IAM_ROLE"]["ARN"])
+
+
+staging_songs_copy = ("""
+COPY {} 
+FROM '{}'
+CREDENTIALS 'aws_iam_role={}'
+FORMAT AS JSON 'auto'
+REGION 'US-WEST-2';
+""").format(table_names_config["STAGING"]["songs"],
+            config["S3"]["SONG_DATA"],
+            config["IAM_ROLE"]["ARN"])
+
+copy_table_queries = [staging_events_copy, staging_songs_copy]
