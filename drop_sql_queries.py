6a13848e963a5cb49aaa604247aa9fa371473ebb
Joseph Corrado <josephcorrado7@gmail.com>
2021-01-18 15:04:20 -0500

    Initial commit on the drop sql queries file. This module contains the SQL queries required to drop all the tables created in this project

---
+++ drop_sql_queries.py
@@ -0,0 +1,23 @@
+"""These are the SQL queries that are used to drop the tables that may have
+already been created
+"""
+
+import configparser
+
+table_names_config = configparser.ConfigParser()
+table_names_config.read("table_names.cfg")
+
+staging_events_table_drop = "DROP TABLE IF EXISTS {}"\
+        .format(table_names_config["STAGING"]["events"])
+staging_songs_table_drop = "DROP TABLE IF EXISTS {}"\
+        .format(table_names_config["STAGING"]["songs"])
+songplay_table_drop = "DROP TABLE IF EXISTS {}"\
+        .format(table_names_config["FINAL"]["songplays"])
+user_table_drop = "DROP TABLE IF EXISTS {}"\
+        .format(table_names_config["FINAL"]["users"])
+song_table_drop = "DROP TABLE IF EXISTS {}"\
+        .format(table_names_config["FINAL"]["songs"])
+artist_table_drop = "DROP TABLE IF EXISTS {}"\
+        .format(table_names_config["FINAL"]["artists"])
+time_table_drop = "DROP TABLE IF EXISTS {}"\
+        .format(table_names_config["FINAL"]["time"])
